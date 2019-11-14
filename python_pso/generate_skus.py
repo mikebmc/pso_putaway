de34796f36f29c8a4dc7dccf15a415780753ee8b
Michael Mcintosh <mike.b.mcintosh@gmail.com>
2019-11-14 16:51:22 -0500

    add function to mock skus most likely ordered with

--- python_pso/generate_skus.py
+++ python_pso/generate_skus.py
@@ -1,5 +1,7 @@
+import random
+
 class GenerateSkus:
-	def permutateSkus(numOfSkus):
+	def permutateSkus(self,numOfSkus):
 		"""
 		:type numOfSkus: int
 		:rtype: List[string]
@@ -7,28 +9,46 @@ class GenerateSkus:
 		skus = []
 		letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		numbers = [0,1,2,3,4,5,6,7,8,9]
-		permutateHelper(skus,numOfSkus,letters,numbers,[])
+		self.permutateHelper(skus,numOfSkus,letters,numbers,[])
 		return skus
-		
-	def permutateHelper(skus,numOfSkus,letters,numbers,perm):
+
+	def permutateHelper(self,skus,numOfSkus,letters,numbers,perm):
 		if len(skus) == numOfSkus:
 					return
 		if len(perm) == 6:
-			skus.append(perm[:])
+			skus.append(''.join(perm[:]))
 			return
 		else:
-			if len(perm) == 2 and lnp < len(numbers):
+			if len(perm) == 2:
 				for i in range(len(numbers)):
-					perm.append(numbers[i])
-					permutateHelper(skus,numOfSkus,letters,numbers,perm)
+					perm.append(str(numbers[i]))
+					self.permutateHelper(skus,numOfSkus,letters,numbers,perm)
 					perm.pop()
-			elif len(perm) == 5 and rnp < len(numbers):
+			elif len(perm) == 5:
 				for i in range(len(numbers)):
-					perm.append(numbers[i])
-					permutateHelper(skus,numOfSkus,letters,numbers,perm)
+					perm.append(str(numbers[i]))
+					self.permutateHelper(skus,numOfSkus,letters,numbers,perm)
 					perm.pop()
 			else:
 				for i in range(len(letters)):
 					perm.append(letters[i])
-					permutateHelper(skus,numOfSkus,letters,numbers,perm)
+					self.permutateHelper(skus,numOfSkus,letters,numbers,perm)
 					perm.pop()
+
+	def getSkus(self,numOfSkus,numOfAssociated):
+		skuDict = {}
+		skus = self.permutateSkus(numOfSkus)
+		for i in range(numOfSkus):
+			associatedDict = {}
+			sku = skus[i]
+			while len(associatedDict) < numOfAssociated:
+				randSku = skus[random.randint(0,numOfSkus-1)]
+				if randSku not in associatedDict and randSku != sku:
+					associatedDict[randSku] = round(random.random(),3)
+			skuDict[sku] = associatedDict
+		return skuDict
+
+
+#skus = GenerateSkus().getSkus(50,3)
+#for key,val in skus.items():
+#	print(key,':',val)
