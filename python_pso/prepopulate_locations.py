ffedaab1f1d6a4e8d9cc9d3bbbdc6da28966d64f
Michael Mcintosh <mike.b.mcintosh@gmail.com>
2019-12-05 09:10:30 -0500

    start prepopulate function
    Commit before refactoring to treat sku as an object

--- python_pso/prepopulate_locations.py
+++ python_pso/prepopulate_locations.py
@@ -5,9 +5,21 @@ random skus until the locations are some percentage full
 """
 
 class PrepopulateLocations:
-	def start(self,skus,locations):
+	def start(self,skus,locations,percentFill):
 		"""
 		type skus: Dictionary[string : Dictionary[string : float]]
 		type locations: Dictionary[char : List[string]]
-		rtype: tbd
+		type percentFill: float
+		rtype: Dictionary[string : List[string]]
+		TODO: check if locations are passed by reference
 		"""
+		
+		index = locations["index"]
+		locationsPer = len(locations[index[0]])
+		totalLocations = len(index)*locationsPer
+		numOfSkus = len(skus)
+		
+		if numOfSkus < totalLocations:
+			#self.fillByMultipleSkus
+		else:
+			#self.fillBySingleSku(skus,locations,index,locationsPer)
